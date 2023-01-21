a3280cdb7e3f7f7cf4018e39e8bce38d2d3bcb6b
Myklon <livroklon@gmail.com>
2023-01-21 18:11:50 +0300

    Implement HTML pages feat: add profile.php feat: edit styles.css

--- public/profile.php
+++ public/profile.php
@@ -31,8 +31,21 @@
 </nav>
 
 <main>
-    <div class="container">
-        <h2 class="page-title">Профиль</h2>
+    <div class="container" style="padding-top: 30px;">
+        <div class="profile-header">
+            <div class="left-section">
+                <img src="https://shapka-youtube.ru/wp-content/uploads/2020/08/man-silhouette.jpg" alt="Profile Picture">
+                <button>Change avatar</button>
+            </div>
+            <div class="right-section">
+                <h1>John Doe</h1>
+                <div class="profile-content">
+                    <p>Дата регистрации: ХХ.ХХ.ХХХХ</p>
+                    <p>Количество постов: 0</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna a faucibus commodo, ipsum velit dictum magna, non ullamcorper erat velit id purus.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 
